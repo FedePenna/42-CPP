f7976de92107a568af48ca0a3ad861c2e5f2adfa
Federico Pennarola <fepennar@c3r5s5.42roma.it>
2026-06-25 17:50:45 +0200

    Ex03

--- CPP01/ex03/HumanA.hpp
+++ CPP01/ex03/HumanA.hpp
@@ -1,14 +1,14 @@
 #pragma once
 
-#include "weapon.hpp"
+#include "Weapon.hpp"
 
 class HumanA
 {
     private:
         std::string name;
-        Weapon& Warhammer;
+        Weapon& weapon;
     public:
-        HumanA(std::string name, Weapon& Warhammer);
+        HumanA(std::string name, Weapon& weapon);
         void    attack() const;
         ~HumanA();
 };
---
+++ CPP01/ex03/HumanB.cpp
@@ -0,0 +1,17 @@
+#include "HumanB.hpp"
+
+HumanB::HumanB(std::string name) : name(name)
+{
+}
+
+HumanB::~HumanB()
+{
+}
+
+void    HumanB::attack() const {
+    std::cout << name << " attacks with their " << weapon->getType() << std::endl;
+}
+
+void   HumanB::setWeapon(Weapon& weapon) {
+    this->weapon = &weapon;
+}
